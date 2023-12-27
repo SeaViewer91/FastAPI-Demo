e3adc406ac9007da8b734a19dbd62539572304eb
SeaViewer91 <shbak91@kakao.com>
2023-12-27 16:32:41 +0900

    .

--- database.py
+++ database.py
@@ -2,8 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-import contextlib
-
 SQLALCHEMY_DATABASE_URL = "sqlite:///./api.db"  # Project Root에 file로 저장
 
 engine = create_engine(
@@ -23,7 +21,6 @@ Base = declarative_base()
 
 # Dependency Injection(의존성 주입)
 # DB 세션 생성 및 종료
-@contextlib.contextmanager
 def get_db():
     db = SessionLocal()
     
