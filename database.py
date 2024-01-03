13cde3a7f384466299e91c6a21dae41f343c9851
SeaViewer91 <shbak91@kakao.com>
2024-01-03 11:23:19 +0900

    .

--- database.py
+++ database.py
@@ -1,31 +1,47 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from starlette.config import Config
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./api.db"  # Project Root에 file로 저장
+config = Config('.env')
+SQLALCHEMY_DATABASE_URL = config('SQLALCHEMY_DATABASE_URL')
 
-engine = create_engine(
-    url=SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
-
-# DB 접속을 위한 세션 생성
-SessionLocal = sessionmaker(
-    autocommit=False, # commit 사인이 없는 경우 DB를 갱신하지 않음.
-    autoflush=False,
-    bind=engine
-)
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+Base.metadata = MetaData(naming_convention=naming_convention)
 
 
-# Dependency Injection(의존성 주입)
-# DB 세션 생성 및 종료
 def get_db():
     db = SessionLocal()
-    
     try:
         yield db
-    
     finally:
         db.close()
+
+
+# Async database
+SQLALCHEMY_DATABASE_URL_ASYNC = config('SQLALCHEMY_DATABASE_URL_ASYNC')
+async_engine = create_async_engine(SQLALCHEMY_DATABASE_URL_ASYNC)
+
+
+async def get_async_db():
+    db = AsyncSession(bind=async_engine)
+    try:
+        yield db
+    finally:
+        await db.close()
--- models.py
+++ models.py
@@ -1,37 +1,54 @@
-"""
-게시판의 질문과 답변 데이터를 저장하기 위한 모델
-"""
-from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
 from database import Base
 
+question_voter = Table(
+    'question_voter',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('user.id'), primary_key=True),
+    Column('question_id', Integer, ForeignKey('question.id'), primary_key=True)
+)
 
-"""
-pip install alembic
+answer_voter = Table(
+    'answer_voter',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('user.id'), primary_key=True),
+    Column('answer_id', Integer, ForeignKey('answer.id'), primary_key=True)
+)
 
-모델 정의 후 'alembic revision --autogenerate' 명령어로 리비전 파일 생성
-이후 'alembic upgrade head' 명령어로 리비전 파일 실행
-
-main.py에 models.Base.metadata.create_all(bind=engine) 코드 추가 시
-alembic 없이 테이블 생성이 가능하나, 이경우 한번 생성된 테이블에 대한 변경관리가 불가
-"""
 
 class Question(Base):
     __tablename__ = "question"
-    
-    id = Column(Integer, primary_key=True)  # 질문 데이터의 고유번호
-    subject = Column(String, nullable=False)  # 질문 게시물의 제목
-    content = Column(Text, nullable=False)  # 질문 내용
-    create_date = Column(DateTime, nullable=False)  # 질문 작성일시
-    
+
+    id = Column(Integer, primary_key=True)
+    subject = Column(String, nullable=False)
+    content = Column(Text, nullable=False)
+    create_date = Column(DateTime, nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=True)
+    user = relationship("User", backref="question_users")
+    modify_date = Column(DateTime, nullable=True)
+    voter = relationship('User', secondary=question_voter, backref='question_voters')
+
 
 class Answer(Base):
     __tablename__ = "answer"
-    
-    id = Column(Integer, primary_key=True)  # 답변 데이터의 고유번호
-    content = Column(Text, nullable=False)  # 답변 내용
-    create_date = Column(DateTime, nullable=False)  # 답변 작성일시  
-    question_id = Column(Integer, ForeignKey("question.id"))  # 질문 데이터의 고유번호(외래키)
-    question = relationship("Question", backref="answers")  # 하나의 질문에 여러 답변이 생성되는 것을 고려하여 역참조 설정
 
+    id = Column(Integer, primary_key=True)
+    content = Column(Text, nullable=False)
+    create_date = Column(DateTime, nullable=False)
+    question_id = Column(Integer, ForeignKey("question.id"))
+    question = relationship("Question", backref="answers")
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=True)
+    user = relationship("User", backref="answer_users")
+    modify_date = Column(DateTime, nullable=True)
+    voter = relationship('User', secondary=answer_voter, backref='answer_voters')
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
